7712d94812a3d5a5ce27d155b8026f6090f3390d
Xin訫 <109326332+xieqsei@users.noreply.github.com>
2025-09-20 22:08:56 +0800

    Update config.ts
    测试

--- src/config.ts
+++ src/config.ts
@@ -18,10 +18,10 @@ import { getTranslateLanguageFromConfig } from "./utils/language-utils";
 // 移除i18n导入以避免循环依赖
 
 // 定义站点语言
-const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
+const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
-	title: "Mizuki",
+	title: "Mizuki测试",
 	subtitle: "One demo website",
 
 	lang: SITE_LANG,
